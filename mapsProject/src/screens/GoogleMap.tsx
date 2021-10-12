c7a8a2aba559e1b12b025ba161ea8626a75a1626
bayar <bayar@nest-33s-iMac.local>
2021-10-12 12:42:33 +0800

    maps working

--- mapsProject/src/screens/GoogleMap.tsx
+++ mapsProject/src/screens/GoogleMap.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import React, {useRef, useState} from 'react';
+import MapView, {Marker, LatLng, PROVIDER_GOOGLE} from 'react-native-maps';
 import {View, StyleSheet} from 'react-native';
 
 export const GoogleMap = () => {
+  const mapRef = useRef(null);
+  const [markers, setMarkers] = useState<LatLng[]>([]);
+
+  const _renderMarkers = () => {
+    console.log(markers);
+    return markers.map((marker, index) => (
+      <Marker
+        key={index}
+        coordinate={{
+          latitude: marker.latitude,
+          longitude: marker.longitude,
+        }}
+      />
+    ));
+  };
   return (
     <View style={styles.container}>
       <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        ref={mapRef}
+        provider={PROVIDER_GOOGLE}
         style={styles.map}
+        onPress={event => {
+          const {latitude, longitude} = event.nativeEvent.coordinate;
+          setMarkers(prev => [...prev, {latitude, longitude}]);
+        }}
         region={{
           latitude: 47.91972,
           longitude: 106.90916,
           latitudeDelta: 0.075,
           longitudeDelta: 0.075,
         }}>
-        <Marker
-          coordinate={{
-            latitude: 47.91972,
-            longitude: 106.90916,
-          }}
-        />
+        {_renderMarkers()}
       </MapView>
     </View>
   );
